5988fc597e3e914da118a51cd97ed54e8440d8bf
daffa03x <mobadaff@gmail.com>
2024-11-22 10:15:28 +0700

    ubah ulang config db

--- config/index.js
+++ config/index.js
@@ -6,5 +6,5 @@ dotenv.config();
 module.exports = {
   rootPath: path.resolve(__dirname, ".."),
   serviceName: process.env.SERVICE_NAME,
-  urlDb: "mongodb+srv://daffa03x:085123dafa@cluster0.imtcis6.mongodb.net/?retryWrites=true&w=majority",
+  urlDb: process.env.MONGO_URL,
 };
