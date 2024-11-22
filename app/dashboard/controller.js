76c2ea1e731e3c54d1df2d77b788f5b1d7955d00
daffa03x <mobadaff@gmail.com>
2024-11-22 10:00:35 +0700

    hapus isAdmin dashboard

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -2,7 +2,7 @@ module.exports = {
   index: async (req, res) => {
     try {
       res.render("admin/dashboard/view_dashboard", {
-        name: req.session.user.name,
+        // name: req.session.user.name,
         title: "Halaman Dashboard",
       });
     } catch (err) {
