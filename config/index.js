8beab3b13a5ff16596d889afb8eb8d3bf1809081
daffa03x <mobadaff@gmail.com>
2024-11-22 10:08:05 +0700

    ubah config db tidak melalui .env

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
-const dotenv = require('dotenv');
-const path = require('path')
+const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 
 module.exports = {
-    rootPath : path.resolve(__dirname, '..'),
-    serviceName: process.env.SERVICE_NAME,
-    urlDb: process.env.MONGO_URL
-}
+  rootPath: path.resolve(__dirname, ".."),
+  serviceName: process.env.SERVICE_NAME,
+  urlDb: "mongodb+srv://daffa03x:085123dafa@cluster0.imtcis6.mongodb.net/?retryWrites=true&w=majority",
+};
